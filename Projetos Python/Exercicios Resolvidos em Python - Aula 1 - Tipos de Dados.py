976764727806243411e6c03fa8f4b369debb4461
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2023-03-08 23:28:30 -0300

    Create Exercicios Resolvidos em Python - Aula 1 - Tipos de Dados.py

---
+++ Projetos Python/Exercicios Resolvidos em Python - Aula 1 - Tipos de Dados.py
@@ -0,0 +1,19 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> cumprimento = "Olá"
+>>> nome = "Ana"
+>>> turno = "Bom dia"
+>>> len(turno)
+7
+>>> len(nome)
+3
+>>> len(cumprimento)
+3
+>>> 22%3
+1
+>>> 2*1**2
+2
+>>> tamanho = len(cumprimento) + len(nome) + len(turno)
+>>> tamanho
+13
+>>> 
